543e3d7ef94ad003315ac569bba7d7671273198f
Matheus Peixoto <matheus.peixoto@townsq.com.br>
2024-09-30 20:08:15 -0300

    changed error body to problem detail

--- src/main/java/com/iff/sistema_gerenciamento_hospital/domain/exceptions/MapeadorExcecao.java
+++ src/main/java/com/iff/sistema_gerenciamento_hospital/domain/exceptions/MapeadorExcecao.java
@@ -1,6 +1,7 @@
 package com.iff.sistema_gerenciamento_hospital.domain.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class MapeadorExcecao {
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorBody handleNotFoundException(NotFoundException e) {
-        return new ErrorBody(HttpStatus.NOT_FOUND, e.getMessage());
+    public ProblemDetail handleNotFoundException(NotFoundException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
     }
 
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorBody handlePreConditionFailedException(BadRequestException e) {
-        return new ErrorBody(HttpStatus.PRECONDITION_FAILED, e.getMessage());
+    public ProblemDetail handlePreConditionFailedException(BadRequestException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 }
